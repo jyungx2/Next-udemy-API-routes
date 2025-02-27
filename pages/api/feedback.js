6f7d7d20c500e96fac53a781a09c01572f89db41
jiyoungnim <cloundyon31@gmail.com>
2025-02-27 20:35:01 +0900

    📝 321. Parsing The Incoming Request & Executing Server-side Code

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,36 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
-  res.status(200).json({ message: "This works!" });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    // store that in a db or in a file
+    // 데이터를 저장할 파일 경로 설정
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+
+    // 기존 데이터 읽기
+    const fileData = fs.readFileSync(filePath); // 파일이 존재하면 기존 데이터를 읽어오고, 존재하지 않으면 빈 배열을 기본값으로 설정
+    const data = JSON.parse(fileData); // 기존 데이터를 JSON으로 변환한 후 새로운 피드백을 추가
+
+    // 새로운 데이터 추가
+    data.push(newFeedback);
+
+    // 파일에 다시 저장
+    fs.writeFileSync(filePath, JSON.stringify(data)); // 데이터 보관할 파일에 데이터를 JSON 스트링화하여 추가 (fs.writeFileSync 함수로 파일을 덮어씀)
+
+    // 성공 응답 반환
+    res.stats(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "This works!" });
+  }
 }
 
 export default handler;
